8fd1708751cf43f39e9f90375a77be40694290a1
Renzo Aguirre <83506694+renzo-aguirre@users.noreply.github.com>
2021-05-16 09:42:13 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,36 +2,44 @@ var l_number = document.getElementById("l_number");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", drwForClick);
 
+var btn = document.getElementById("c_btn");
+btn.addEventListener("click", drwClear);
+
 var b = document.getElementById("board");
 var b_width = b.width;
 var b_height = b.height;
 var canvas = b.getContext("2d");
+var l_color = "black";
+
+drwLine(l_color, 1, 1, 1, 499);
+drwLine(l_color, 1, 499, 499, 499);
+
+drwLine(l_color, 499, 1, 1, 1);
+drwLine(l_color, 499, 499, 499, 1);
 
 function drwForClick()
 {
     var lines = parseInt(l_number.value);
     var l = 0;
-    var l_color = "black";
     var space = b_width / lines;
 
 for(l = 0; l < lines; l++)
 {
     yi = space * l;
     xf = space * (l + 1);
-    drwLine("#36393E", 0, yi, xf, 500);
+    drwLine("black", 0, yi, xf, 500);
 }
 
 for(l = 0; l < lines; l++)
 {
     yi = space * l;
     xf = space * (l + 1);
-    drwLine("#36393E", yi, 0, 500, xf);
+    drwLine("black", yi, 0, 500, xf);
 }
 
-drwLine(l_color, 1, 1, 1, 499);
-drwLine(l_color, 1, 499, 499, 499);
-
 }
+
+
 
 function drwLine(color, x_initial, y_initial, x_final, y_final)
 {
@@ -43,3 +51,24 @@ function drwLine(color, x_initial, y_initial, x_final, y_final)
     canvas.closePath();
 }
 
+function drwClear()
+{
+    var lines = parseInt(l_number.value);
+    var l = 0;
+    var space = b_width / lines;
+
+for(l = 0; l < lines; l++)
+{
+    yi = space * l;
+    xf = space * (l + 1);
+    drwLine("white", 0, yi, xf, 500);
+}
+
+for(l = 0; l < lines; l++)
+{
+    yi = space * l;
+    xf = space * (l + 1);
+    drwLine("white", yi, 0, 500, xf);
+}
+
+}
